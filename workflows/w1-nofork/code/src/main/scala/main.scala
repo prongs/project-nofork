e08e3e74719a3937f9ce2d261f97872ea252f152
rajatgupta59@gmail.com <rajatgupta59@gmail.com>
2021-10-08 11:45:28 +0000

    lkjlk

---
+++ workflows/w1-nofork/code/src/main/scala/main.scala
@@ -0,0 +1,32 @@
+import io.prophecy.libs._
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import config.ConfigStore._
+import udfs.UDFs._
+import udfs._
+import graph._
+
+object Main {
+
+  def apply(spark: SparkSession): Unit = {
+    val df_source_0_out0 = source_0(spark)
+    df_source_0_out0.cache().count()
+    df_source_0_out0.unpersist()
+  }
+
+  def main(args: Array[String]): Unit = {
+    import config._
+    ConfigStore.Config = ConfigurationFactoryImpl.fromCLI(args)
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName("Prophecy Workflow")
+      .config("spark.default.parallelism", "4")
+      .enableHiveSupport()
+      .getOrCreate()
+    spark.sparkContext.setCheckpointDir("/tmp/checkpoints")
+    apply(spark)
+  }
+
+}
