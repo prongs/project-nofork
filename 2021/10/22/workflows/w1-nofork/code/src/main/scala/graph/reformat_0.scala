36a0290d7bce23c6e392b94e27cd94a32c7074d3
rajatgupta59@gmail.com <rajatgupta59@gmail.com>
2021-10-25 13:47:42 +0000

    Workflow Code Updated

--- 2021/10/22/workflows/w1-nofork/code/src/main/scala/graph/reformat_0.scala
+++ 2021/10/22/workflows/w1-nofork/code/src/main/scala/graph/reformat_0.scala
@@ -12,13 +12,14 @@ import udfs._
 object reformat_0 {
 
   def apply(spark: SparkSession, in: DataFrame): DataFrame =
-    in.select(col("customer_id"),
-              col("first_name"),
-              col("last_name"),
-              col("last_name"),
-              col("last_name"),
-              col("phone"),
-              col("email")
+    in.select(
+      col("customer_id"),
+      col("first_name"),
+      col("last_name"),
+      struct(col("customer_id"), lit("40").as("phoneNumber")).as("aaa"),
+      struct(col("customer_id"), lit("40").as("phoneNumber")).as("aaa"),
+      col("phone"),
+      col("email")
     )
 
 }
