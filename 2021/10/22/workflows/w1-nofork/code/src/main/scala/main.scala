2b57d822535d87b3cc1f5b6d3b82fa4b99204e53
rajatgupta59@gmail.com <rajatgupta59@gmail.com>
2021-10-25 13:43:37 +0000

    Workflow Code Updated

--- 2021/10/22/workflows/w1-nofork/code/src/main/scala/main.scala
+++ 2021/10/22/workflows/w1-nofork/code/src/main/scala/main.scala
@@ -11,7 +11,8 @@ import graph._
 object Main {
 
   def apply(spark: SparkSession): Unit = {
-    val df_source_0 = source_0(spark)
+    val df_source_0   = source_0(spark)
+    val df_reformat_0 = reformat_0(spark)
   }
 
   def main(args: Array[String]): Unit = {
